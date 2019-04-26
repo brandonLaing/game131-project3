1b886b5ff9cac245e4c83871b708dc8c8eaaf178
brandonLaing <38956313+brandonLaing@users.noreply.github.com>
2019-04-25 19:19:31 -0500

    Made revolving list

---
+++ RevolvingList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class RevolvingList<T>
+{
+    private List<T> _list;
+    private int _currentIndex = 0;
+    private int currentIndex
+    {
+        get
+        {
+            return _currentIndex;
+        }
+        set
+        {
+            _currentIndex = value;
+            if (_currentIndex > _list.Count)
+                _currentIndex = 0;
+        }
+    }
+
+    public RevolvingList(List<T> newList)
+    {
+        if (newList == null || newList.Count < 2)
+            throw new Exception("Revolving list needs to have at least 2 points");
+    
+        _list = newList;
+    }
+
+    public T Next
+    {
+        get
+        {
+            return _list[currentIndex++];
+        }
+    }
+
+    public T Start
+    {
+        get
+        {
+            return _list[0];
+        }
+    }
+}
